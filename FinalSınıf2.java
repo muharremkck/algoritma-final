b9e9506586de62dcc52745bd014d199e1fd8aae2
muharremkck <muharremkucuk16049@gmail.com>
2025-05-13 11:07:50 +0300

    FinalSınıf2.java

---
+++ FinalSınıf2.java
@@ -0,0 +1,14 @@
+package NesneYonelımlıProgrsmlama;
+
+public class FinalSınıf2 {
+     
+	public String zaman,giyecek ,gıda ,kırtasiye,alan,yer,ısımBılgısı;
+	public  String telno,tc;
+	     public int input,miktar;
+	       
+	            public int a;
+	
+	         //public   int[] tc = new int[5];
+	
+}
+   
